bc0ed34bfb2615c859446ef267fd3b5d44acb558
Adéla Bierská <xbiersk1@fi.muni.cz>
2023-03-31 17:58:47 +0200

    project: Init structure, install packages, import functions from notebook

---
+++ project/Visualizations/Plots.fs
@@ -0,0 +1,13 @@
+﻿namespace EuropeanCoffee.Visualizations
+
+module Plots =
+    let columnCounts (df: Frame<int,string>) column =
+        df
+        |> Frame.getCol column
+        |> Series.values
+        |> Seq.map (fun (row: string) -> row.Split(", "))
+        |> Seq.concat
+        |> Seq.countBy id
+        // |> Seq.iter (fun (variety, count) -> printfn "%s %i" variety count)
+        |> Chart.Column
+        |> Chart.Show
